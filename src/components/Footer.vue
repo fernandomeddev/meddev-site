072729bfcae9769cce56a4638729cfd7737c5d11
Fernando Medeiros <fernandohenrique520@live.com>
2021-03-29 08:50:46 -0300

    hello world

---
+++ src/components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+    <footer>
+
+        <a class="socialLink" v-for="(socialLink,index) in socialLinks" :key="index" :href="socialLink.link" >
+            <img :src="socialLink.icon" :title="socialLink.label" :alt="socialLink.label">
+        </a>
+
+    </footer>
+</template>
+
+<script>
+import api from '@/services/api.js';
+
+export default {
+    name: 'Footer',
+    data(){
+        return{
+            socialLinks: []
+        }
+
+    },
+    mounted(){
+        api.get('/social-links.json').then(response => { 
+            this.socialLinks = response.data;
+        });
+    }
+}
+</script>
+
+<style scoped>
+
+    footer {
+        height: 60px;
+        width: 100%;
+        background-color: var(--color-background-nav);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+    .socialLink {
+        border:2px solid var (--color-text-light);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        margin: 10px;
+    }
+
+    .socialLink img{ 
+        width: 20px;
+
+
+    }
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <main>
+        <div class="text-top">Vídeos novos toda <b>quinta</b> às <b>20h</b></div>
+
+        <a href="https://www.youtube.com/channel/UCPtOwBPlYOQTDaCUxUyGSSA" target="blank">
+        <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/subscribe.svg
+" alt="inscreva-se" id="subscribe">
+        
+        </a>
+
+        <div class="text-bottom">youtube.com/desenvtioficial</div>
+    </main>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+    main{
+        background-color: var(--color-background-home);
+        justify-content: center;
+        align-items: center;
+    }
+
+    .text-top{
+        color: var(--color-text-ligth);
+        font-size: 22px;
+        margin-bottom: 20px;
+
+    }
+
+    #subcribe{
+        width: 340px;
+    }
+    
+    .text-bottom{
+        color: var(--color-text-light);
+        font-size: 18px;
+        margin-top: 20px;
+    }
+
+    @media (min-width:700px){
+        #subscribe{
+            width: 370px;
+        }
+    }
+
+</style>
